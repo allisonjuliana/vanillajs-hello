9a54eed9231bf9065a05c2a19f4da7f0849ff1c3
allisonjuliana <162182668+allisonjuliana@users.noreply.github.com>
2024-11-22 00:59:56 +0000

    generador de nombres

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//write your code here
+// console.log("Hello Rigo from the console!");
+// };
+
+let pronoun = ["yo", "tu", "esto"];
+let adj = [
+  "el más alto",
+  "el más bajito",
+  "el más fuerte",
+  "el más ágil",
+  "el más débil"
+];
+let noun = ["comerciante", "ciudadano", "atleta", "marido", "amante"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(`${pronoun[i]} ${adj[j]} ${noun[k]}.com`);
+    }
+  }
+}
